9771777ae36c509061a3c7c26b13722dcf9c310b
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-06-18 19:05:44 +0530

    [Yash Agrawal].add exception handling quesitons done

---
+++ core-java-practice/gcr-codebase/JavaExceptionHandling/practice_problems/ArrayExceptionHandling.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ArrayExceptionHandling {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Integer[] array = {10, 20, 30, 40, 50};
+
+        try {
+
+            System.out.print("Enter index: ");
+            int index = sc.nextInt();
+
+            System.out.println(
+                    "Value at index "
+                            + index
+                            + ": "
+                            + array[index]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Invalid index!");
+
+        } catch (NullPointerException e) {
+
+            System.out.println(
+                    "Array is not initialized!");
+        }
+
+        sc.close();
+    }
+}
---
+++ core-java-practice/gcr-codebase/JavaExceptionHandling/practice_problems/NestedTryCatchDemo.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class NestedTryCatchDemo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = {10, 20, 30, 40, 50};
+
+        try {
+
+            System.out.print("Enter Index: ");
+            int index = sc.nextInt();
+
+            try {
+
+                System.out.print(
+                        "Enter Divisor: ");
+                int divisor = sc.nextInt();
+
+                int result =
+                        array[index] / divisor;
+
+                System.out.println(
+                        "Result = " + result);
+
+            } catch (ArithmeticException e) {
+
+                System.out.println(
+                        "Cannot divide by zero!");
+            }
+
+        } catch (
+                ArrayIndexOutOfBoundsException e) {
+
+            System.out.println(
+                    "Invalid array index!");
+        }
+
+        sc.close();
+    }
+}
