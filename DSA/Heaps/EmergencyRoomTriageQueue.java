b67bb7056ea97bd52a25bf2642ba8fbaac6bf459
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-07-21 23:44:56 +0530

    [Yash Agrawal].add Heaps scenario based questions completed

---
+++ DSA/Heaps/EmergencyRoomTriageQueue.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+
+class Patient {
+    int severity;
+    String name;
+
+    public Patient(int severity, String name) {
+        this.severity = severity;
+        this.name = name;
+    }
+}
+
+public class EmergencyRoomTriageQueue {
+    private PriorityQueue<Patient> triageQueue;
+
+    public EmergencyRoomTriageQueue() {
+        this.triageQueue = new PriorityQueue<>((a, b) -> b.severity - a.severity);
+    }
+
+    public void addPatient(Patient p) {
+        triageQueue.offer(p);
+    }
+
+    public Patient treatNext() {
+        return triageQueue.poll();
+    }
+}
